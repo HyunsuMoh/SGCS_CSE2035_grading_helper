289ac02ae871d216738d3cdf33e5b4d72a38e21e
Hyunsu Moh <hyunsumo@gmail.com>
2021-10-01 23:27:36 +0900

    Before test

--- create_workspace.sh
+++ create_workspace.sh
@@ -1,14 +1,17 @@
 targetdir=$(dirname $0)
 if [ $# -eq 1 ]
 then
-	if [ -f $targetdir/$1 ]
+	if [ -f $1 ]
 	then
 		echo "The file $1 exists."
 	else
-		mkdir $targetdir/$1
-		mkdir $targetdir/$1/codes
-		cp $targetdir/resources/Makefile $targetdir/$1/
-		cp $targetdir/resources/student_list.txt $targetdir/$1
+		mkdir $1
+		mkdir $1/codes
+		mkdir $1/testcases
+		mkdir $1/executables
+		mkdir $1/results
+		cp $targetdir/resources/Makefile $1
+		cp $targetdir/resources/student_list.txt $1
 	fi
 
 else
