7c874e6d7c00236db2dafce45b98436b1790b99b
Hyunsu Moh <hyunsumo@gmail.com>
2021-10-02 02:53:19 +0900

    Now works well with multiple testcases

--- resources/grade.sh
+++ resources/grade.sh
@@ -1,16 +1,22 @@
-rm total.result
+rm -f total.result
 for code in $(ls codes)
 do
-# Build & Run
+# Build
 	id=${code%.c}
 	gcc -o executables/$id.out codes/$code
-	executables/$id.out > results/$id.result
 
 # Show the result
 	echo "ID: $id\n" >> total.result
+	echo "Results:" >> total.result
+	rm -f results/$id.result
+	for testcase in $(ls testcases)
+	do
+		echo "$testcase" >> results/$id.result
+		executables/$id.out < testcases/$testcase >> results/$id.result
+		echo "" >> results/$id.result
+	done
+	cat results/$id.result >> total.result
 	echo "Source code:" >> total.result
 	cat codes/$code >> total.result
-	echo "\nResults:" >> total.result
-	cat results/$id.result >> total.result
 	echo "\n" >> total.result
 done
